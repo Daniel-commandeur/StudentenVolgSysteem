2c104aae66c8fe4e429e5f02da2c8ba8261362cb
eiedu <gsfeied@gmail.com>
2019-09-12 15:53:41 +0200

    Added Summaries to the private ExcelController methods

--- StudentenVolgSysteem/Controllers/ExcelController.cs
+++ StudentenVolgSysteem/Controllers/ExcelController.cs
@@ -18,6 +18,11 @@ namespace StudentenVolgSysteem.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Reads and parses data from a $-separated file and writes the parsed dropdown models into the database
+        /// </summary>
+        /// <param name="db">The database context to write the dropdown models to</param>
+        /// <param name="filePath">The path to the $-separated file</param>
         private void GetDropdownSheetData(MyDbContext db, string filePath)
         {
             //string pathToSheetTwo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Data\InfraWorkshopsSheet2.csv";
@@ -64,6 +69,11 @@ namespace StudentenVolgSysteem.Controllers
             }
         }
 
+        /// <summary>
+        /// Reads and parses data from a $-separated file and writes the parsed Topics into the database
+        /// </summary>
+        /// <param name="db">The database context to write the TopicModels to</param>
+        /// <param name="filePath">The path to the $-separated file</param>
         private void GetTopicSheetData(MyDbContext db, string filePath)
         {
             //string pathToSheetOne = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Data\InfraWorkshopsSheet1.csv";
@@ -182,12 +192,18 @@ namespace StudentenVolgSysteem.Controllers
             }
         }
 
-        private HashSet<TopicModel> FindVoorkennis(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt de topics die als voorkennis nodig zijn.
+        /// </summary>
+        /// <param name="topicsString">Een string met daarin de codes van TopicModels die met LineFeeds gescheiden zijn</param>
+        /// <param name="db">De database context om de TopicModels in te vinden</param>
+        /// <returns>De HashSet van TopicModels die gevonden zijn</returns>
+        private HashSet<TopicModel> FindVoorkennis(string topicsString, MyDbContext db)
         {
             //split the cell on newline and find all links that match, 
             //make new entries for anythign that doesn't match and add it.
             HashSet<TopicModel> voorkennis = new HashSet<TopicModel>();
-            var entries = splitTopicEntry.Split('\n').ToList();
+            var entries = topicsString.Split('\n').ToList();
             foreach (var item in entries)
             {
                 TopicModel tpm;
@@ -200,12 +216,19 @@ namespace StudentenVolgSysteem.Controllers
             return voorkennis;
         }
 
-        private HashSet<PercipiolinkModel> FindLinks(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt de PercipiolinkModels die in links voorkomen.
+        /// Maakt de PercipiolinkModels die niet gevonden kunnen worden.
+        /// </summary>
+        /// <param name="links">Een string met links die door LineFeeds gescheiden zijn</param>
+        /// <param name="db">De database context om de PercipiolinkModels in aan te maken</param>
+        /// <returns>De HashSet van PercipiolinkModels die gevonden of gemaakt zijn</returns>
+        private HashSet<PercipiolinkModel> FindLinks(string links, MyDbContext db)
         {
             //split the cell on newline and find all links with that match, 
             //make new entries for anything that doesn't match and add them
             HashSet<PercipiolinkModel> percipiolinks = new HashSet<PercipiolinkModel>();
-            var entries = splitTopicEntry.Split('\n').ToList();
+            var entries = links.Split('\n').ToList();
             foreach (var item in entries)
             {
                 PercipiolinkModel plm;
@@ -218,26 +241,39 @@ namespace StudentenVolgSysteem.Controllers
             return percipiolinks;
         }
 
-        private PercipiolinkModel MakePercipioLink(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een PercipiolinkModel van link
+        /// </summary>
+        /// <param name="link">De link die in het model moet komen</param>
+        /// <param name="db">De database context om het PercipiolinkModel in aan te maken</param>
+        /// <returns>Het gemaakte PercipiolinkModel</returns>
+        private PercipiolinkModel MakePercipioLink(string link, MyDbContext db)
         {
-            if (db.PercipioLinks.Where(a => a.Link == item).First() == null)
+            if (db.PercipioLinks.Where(a => a.Link == link).First() == null)
             {
                 try
                 {
-                    db.PercipioLinks.Add(new PercipiolinkModel { Link = item });
+                    db.PercipioLinks.Add(new PercipiolinkModel { Link = link });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.PercipioLinks.Where(a => a.Link == item).First();
+            return db.PercipioLinks.Where(a => a.Link == link).First();
         }
 
-        private HashSet<BenodigdheidModel> FindBenodigdheden(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt de BenodigdheidModels die in benodigdhedenString voorkomen.
+        /// Maakt de BenodigdheidModels die niet gevonden kunnen worden.
+        /// </summary>
+        /// <param name="benodigdhedenString">Een string met benodigdheden die door LineFeeds gescheiden zijn</param>
+        /// <param name="db">De database context om de benodigdheden in te vinden</param>
+        /// <returns>De HashSet van BenodigdheidModel met daarin de gevonden of gemaakte modellen</returns>
+        private HashSet<BenodigdheidModel> FindBenodigdheden(string benodigdhedenString, MyDbContext db)
         {
             //split the cell on newline and find all links that match, 
             //make new entries for anythign that doesn't match and add it.
             HashSet<BenodigdheidModel> benodigdheden = new HashSet<BenodigdheidModel>();
-            var entries = splitTopicEntry.Split('\n').ToList();
+            var entries = benodigdhedenString.Split('\n').ToList();
             foreach (var item in entries)
             {
                 BenodigdheidModel bhm;
@@ -250,26 +286,39 @@ namespace StudentenVolgSysteem.Controllers
             return benodigdheden;
         }
 
-        private BenodigdheidModel MakeBenodigdheid(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een BenodigdheidModel aan met content
+        /// </summary>
+        /// <param name="content">De content van het BenodigdheidModel</param>
+        /// <param name="db">De database om het BenodigdheidModel in aan te maken</param>
+        /// <returns>Het aangemaakte BenodigdheidModel</returns>
+        private BenodigdheidModel MakeBenodigdheid(string content, MyDbContext db)
         {
-            if (db.Benodigdheden.Where(a => a.Content == item).First() == null)
+            if (db.Benodigdheden.Where(a => a.Content == content).First() == null)
             {
                 try
                 {
-                    db.Benodigdheden.Add(new BenodigdheidModel { Content = item });
+                    db.Benodigdheden.Add(new BenodigdheidModel { Content = content });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.Benodigdheden.Where(a => a.Content == item).First();
+            return db.Benodigdheden.Where(a => a.Content == content).First();
         }
 
-        private HashSet<CertificeringenInfraModel> FindCerts(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt de certificeringen die in certifications voorkomen.
+        /// Maakt de certificeringen aan die niet gevonden kunnen worden.
+        /// </summary>
+        /// <param name="certifications">Een string met certificaties die door LineFeeds gescheiden zijn</param>
+        /// <param name="db">De database context om de certificeringen in te vinden</param>
+        /// <returns>De HashSet van CertificeringenInfraModel met daarin de gevonden of gemaakte modellen</returns>
+        private HashSet<CertificeringenInfraModel> FindCerts(string certifications, MyDbContext db)
         {
             //split the cell on newline and find all links that match, 
             //make new entries for anythign that doesn't match and add it.
             HashSet<CertificeringenInfraModel> certs = new HashSet<CertificeringenInfraModel>();
-            var entries = splitTopicEntry.Split('\n').ToList();
+            var entries = certifications.Split('\n').ToList();
             foreach (var item in entries)
             {
                 CertificeringenInfraModel cim;
@@ -282,114 +331,172 @@ namespace StudentenVolgSysteem.Controllers
             return certs;
         }
 
-        private CertificeringenInfraModel MakeCert(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een CertificeringenInfraModel aan met certificering
+        /// </summary>
+        /// <param name="certificering">De certificering om aan te maken</param>
+        /// <param name="db">De database context om de certificering in aan te maken</param>
+        /// <returns>Het aangemaakte CertificeringenInfraModel</returns>
+        private CertificeringenInfraModel MakeCert(string certificering, MyDbContext db)
         {
-            if (db.CertificeringenInfras.Where(a => a.Certificering == item).First() == null)
+            if (db.CertificeringenInfras.Where(a => a.Certificering == certificering).First() == null)
             {
                 try
                 {
-                    db.CertificeringenInfras.Add(new CertificeringenInfraModel { Certificering = item });
+                    db.CertificeringenInfras.Add(new CertificeringenInfraModel { Certificering = certificering });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.CertificeringenInfras.Where(a => a.Certificering == item).First();
+            return db.CertificeringenInfras.Where(a => a.Certificering == certificering).First();
         }
 
-        private TagModel FindTag(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt het TagModel dat bij tag hoort.
+        /// Maakt een nieuw TagModel aan in de database als deze niet gevonden kan worden.
+        /// </summary>
+        /// <param name="tag">De tag om te vinden</param>
+        /// <param name="db">De database om de tag in te vinden</param>
+        /// <returns>Het gevonden of gemaakte TagModel</returns>
+        private TagModel FindTag(string tag, MyDbContext db)
         {
             TagModel tmd;
-            if ((tmd = db.Tags.Where(a => a.Naam == splitTopicEntry).First()) == null)
+            if ((tmd = db.Tags.Where(a => a.Naam == tag).First()) == null)
             {
-                tmd = MakeTag(splitTopicEntry, db);
+                tmd = MakeTag(tag, db);
             }
             return tmd;
         }
 
-        private TagModel MakeTag(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een Tag aan met tag
+        /// </summary>
+        /// <param name="tag">De tag om een TagModel van te maken</param>
+        /// <param name="db">De database context om het TagModel in aan te maken</param>
+        /// <returns>Het aangemaakte TagModel</returns>
+        private TagModel MakeTag(string tag, MyDbContext db)
         {
-            if (db.Tags.Where(a => a.Naam == item).First() == null)
+            if (db.Tags.Where(a => a.Naam == tag).First() == null)
             {
                 try
                 {
-                    db.Tags.Add(new TagModel { Naam = item });
+                    db.Tags.Add(new TagModel { Naam = tag });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.Tags.Where(a => a.Naam == item).First();
+            return db.Tags.Where(a => a.Naam == tag).First();
         }
 
-        private WerkvormModel FindWerkvorm(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt het WerkvormModel dat bij de werkvorm hoort.
+        /// Maakt een nieuw WerkvormModel in de database aan als deze niet gevonden kan worden.
+        /// </summary>
+        /// <param name="werkvorm">De werkvorm om te vinden</param>
+        /// <param name="db">De database context om de werkvorm in te vinden</param>
+        /// <returns>Het gevonden of gemaakte WerkvormModel</returns>
+        private WerkvormModel FindWerkvorm(string werkvorm, MyDbContext db)
         {
             WerkvormModel wvm;
-            if ((wvm = db.Werkvormen.Where(a => a.Werkvorm == splitTopicEntry).First()) == null)
+            if ((wvm = db.Werkvormen.Where(a => a.Werkvorm == werkvorm).First()) == null)
             {
-                wvm = MakeWerkvorm(splitTopicEntry, db);
+                wvm = MakeWerkvorm(werkvorm, db);
             }
             return wvm;
         }
 
-        private WerkvormModel MakeWerkvorm(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een Werkvorm aan met werkvorm
+        /// </summary>
+        /// <param name="werkvorm">De werkvorm om een WerkvormModel van te maken</param>
+        /// <param name="db">De database context om het WerkvormModel in aan te maken</param>
+        /// <returns>Het aangemaakte WerkvormModel</returns>
+        private WerkvormModel MakeWerkvorm(string werkvorm, MyDbContext db)
         {
-            if (db.Werkvormen.Where(a => a.Werkvorm == item).First() == null)
+            if (db.Werkvormen.Where(a => a.Werkvorm == werkvorm).First() == null)
             {
                 try
                 {
-                    db.Werkvormen.Add(new WerkvormModel { Werkvorm = item });
+                    db.Werkvormen.Add(new WerkvormModel { Werkvorm = werkvorm });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.Werkvormen.Where(a => a.Werkvorm == item).First();
+            return db.Werkvormen.Where(a => a.Werkvorm == werkvorm).First();
         }
 
-        private TijdsDuurModel FindDuration(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt het TijdsDuurModel dat bij tijdsduur hoort.
+        /// Maakt een nieuw TijdsDuurModel in de database aan als deze niet gevonden kan worden.
+        /// </summary>
+        /// <param name="tijdsduur">De tijdsduur om te vinden</param>
+        /// <param name="db">De database context om de tijdsduur in te vinden</param>
+        /// <returns>Het gevonden of gemaakte TijdsDuurModel</returns>
+        private TijdsDuurModel FindDuration(string tijdsduur, MyDbContext db)
         {
             TijdsDuurModel tdm;
-            if((tdm = db.TijdsDuren.Where(a => a.Eenheid == splitTopicEntry).First()) == null)
+            if((tdm = db.TijdsDuren.Where(a => a.Eenheid == tijdsduur).First()) == null)
             {
-                tdm = MakeDuur(splitTopicEntry, db);
+                tdm = MakeDuur(tijdsduur, db);
             }
             return tdm;
         }
 
-        private TijdsDuurModel MakeDuur(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een TijdsDuurModel aan met tijdsduur
+        /// </summary>
+        /// <param name="tijdsduur">De tijdsduur om een TijdsDuurModel van te maken</param>
+        /// <param name="db">De database context om het TijdsDuurModel in aan te maken</param>
+        /// <returns>Het aangemaakte TijdsDuurModel</returns>
+        private TijdsDuurModel MakeDuur(string tijdsduur, MyDbContext db)
         {
-            if (db.TijdsDuren.Where(a => a.Eenheid == item).First() == null)
+            if (db.TijdsDuren.Where(a => a.Eenheid == tijdsduur).First() == null)
             {
                 try
                 {
-                    db.TijdsDuren.Add(new TijdsDuurModel { Eenheid = item });
+                    db.TijdsDuren.Add(new TijdsDuurModel { Eenheid = tijdsduur });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.TijdsDuren.Where(a => a.Eenheid == item).First();
+            return db.TijdsDuren.Where(a => a.Eenheid == tijdsduur).First();
         }
 
-        private NiveauModel FindNiveau(string splitTopicEntry, MyDbContext db)
+        /// <summary>
+        /// Vindt het NiveauModel dat bij de niveau parameter hoort.
+        /// Maakt een nieuw NiveauModel in de database aan als deze niet gevonden kan worden.
+        /// </summary>
+        /// <param name="niveau">Het niveau om te vinden</param>
+        /// <param name="db">De database context om het niveau in te vinden</param>
+        /// <returns>Het gevonden of gemaakte NiveauModel</returns>
+        private NiveauModel FindNiveau(string niveau, MyDbContext db)
         {
             NiveauModel nvm;
-            if((nvm = db.Niveaus.Where(a => a.Niveau == splitTopicEntry).First()) == null)
+            if((nvm = db.Niveaus.Where(a => a.Niveau == niveau).First()) == null)
             {
-                nvm = MakeNiveau(splitTopicEntry, db);
+                nvm = MakeNiveau(niveau, db);
             }
             return nvm;
         }
 
-        private NiveauModel MakeNiveau(string item, MyDbContext db)
+        /// <summary>
+        /// Maakt een nieuw NiveauModel met niveau
+        /// </summary>
+        /// <param name="niveau">Het Niveau</param>
+        /// <param name="db">De database context om het NiveauModel in aan te maken</param>
+        /// <returns>Het aangemaakte NiveauModel</returns>
+        private NiveauModel MakeNiveau(string niveau, MyDbContext db)
         {
-            if (db.Niveaus.Where(a => a.Niveau == item).First() == null)
+            if (db.Niveaus.Where(a => a.Niveau == niveau).First() == null)
             {
                 try
                 {
-                    db.Niveaus.Add(new NiveauModel { Niveau = item });
+                    db.Niveaus.Add(new NiveauModel { Niveau = niveau });
                     db.SaveChanges();
                 }
                 catch { }
             }
-            return db.Niveaus.Where(a => a.Niveau == item).First();
+            return db.Niveaus.Where(a => a.Niveau == niveau).First();
         }
 
     }
