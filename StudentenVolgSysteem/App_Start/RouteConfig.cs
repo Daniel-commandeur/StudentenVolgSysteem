eb16d315c28b92dd0c480907f57fa7243d35b423
Bart van Meerkerk <bart@gb-development.nl>
2020-01-23 16:54:57 +0100

    Addition of a working landing/homepage

--- StudentenVolgSysteem/App_Start/RouteConfig.cs
+++ StudentenVolgSysteem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace StudentenVolgSysteem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ StudentenVolgSysteem/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentenVolgSysteem.Controllers
+{
+    [Authorize(Roles = "Administrator, Docent")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+    }
+}
