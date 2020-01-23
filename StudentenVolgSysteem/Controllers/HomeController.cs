afe85a44f304cfbb9c80644f799527afaa82a551
Bart van Meerkerk <bart@gb-development.nl>
2020-01-23 17:26:30 +0100

    change in HomeController

--- StudentenVolgSysteem/Controllers/HomeController.cs
+++ StudentenVolgSysteem/Controllers/HomeController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StudentenVolgSysteem.Models;
+using StudentenVolgSysteem.DAL;
 
 namespace StudentenVolgSysteem.Controllers
 {
     [Authorize(Roles = "Administrator, Docent")]
     public class HomeController : Controller
     {
+        private SVSContext db = new SVSContext();
+
         // GET: Home
         public ActionResult Index()
         {
             return View("Home");
+             = db.GetFromDatabase<Student>
+
+             = db.Student 
         }
     }
 }
