4f89efbc5f736c633d96299f7179a2ec468bfa05
Bart van Meerkerk <bart@gb-development.nl>
2020-02-04 15:39:27 +0100

    buf fixes + minor changes

--- StudentenVolgSysteem/Controllers/HomeController.cs
+++ StudentenVolgSysteem/Controllers/HomeController.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using StudentenVolgSysteem.Models;
-using StudentenVolgSysteem.DAL;
 
 namespace StudentenVolgSysteem.Controllers
 {
-    [Authorize(Roles = "Administrator, Docent")]
     public class HomeController : Controller
     {
-        private readonly SVSContext db = new SVSContext();
-
         // GET: Home
         public ActionResult Index()
         {
-            return View("Home");
+            return View();
         }
     }
 }
