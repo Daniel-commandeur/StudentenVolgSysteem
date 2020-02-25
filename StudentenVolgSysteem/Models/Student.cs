1e82cab124fef80c7412e2ae98f47da25a2f4cf7
Mark Jansen <markwilemail@gmail.com>
2020-02-25 16:15:19 +0100

    fix AanmeldDatum wijzigen op Student-edit pagina

--- StudentenVolgSysteem/Models/Student.cs
+++ StudentenVolgSysteem/Models/Student.cs
@@ -12,7 +12,7 @@ namespace StudentenVolgSysteem.Models
     public class Student : IDeletable
     {
         [Key]
-        public int Id { get; set; }     
+        public int Id { get; set; }
         
         [Required]
         public string Voornaam { get; set; }
@@ -21,33 +21,17 @@ namespace StudentenVolgSysteem.Models
         public string Achternaam { get; set; }
         
         [Required]
-        [DataType(DataType.Date), Display(Name = "Aanmelddatum")]
-        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd-MM-yyyy}")]
-        public DateTime? AanmeldDatum { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        //[DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime AanmeldDatum { get; set; }
         
         [NotMapped]
-        [Display(Name ="Naam")]
+        [Display(Name = "Naam")]
         public string VolledigeNaam { get { return $"{Voornaam} {Achternaam}"; } }
-        
+
         public virtual ICollection<Curriculum> Curricula { get; set; }
-                
+
         public bool IsDeleted { get; set; }
     }
-
-    //[NotMapped]
-    //public class CUStudent : Student
-    //{
-    //    public CUStudent()
-    //    {
-
-    //    }
-
-    //    public CUStudent(Student s)
-    //    {
-    //        this.Curricula = s.Curricula;
-    //        this.StudentId = s.StudentId;
-    //        this.Voornaam = s.Voornaam;
-    //        this.Achternaam = s.Achternaam;
-    //    }
-    //}
 }
